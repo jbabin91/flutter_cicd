1fb8ee1740eb6ea040cfef469970f5d0f3caf7bf
Jace Babin <jbabin91@gmail.com>
2020-01-13 16:21:52 -0700

    Updated test naming conventions
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import './main_dev_test.dart' as mainDev;
-import './main_prod_test.dart' as mainStaging;
-import './main_staging_test.dart' as mainProd;
+import './main_dev_test.dart' as main_dev;
+import './main_prod_test.dart' as main_staging;
+import './main_staging_test.dart' as main_prod;
 
 void main() async {
   group('Main with flavors', () {
-    mainDev.main();
-    mainStaging.main();
-    mainProd.main();
+    main_dev.main();
+    main_staging.main();
+    main_prod.main();
   });
 }
